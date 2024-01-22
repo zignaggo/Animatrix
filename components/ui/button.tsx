dc4bec09388a94b6f24a2c0ab6b6a95e06062260
zignaggo <jvz1@aluno.ifal.edu.br>
2024-01-22 08:59:38 -0300

    fix: button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 gap-2',
     {
         variants: {
             variant: {
@@ -20,9 +20,9 @@ const buttonVariants = cva(
                 success: 'bg-lemon-700 hover:bg-lemon-800 active:bg-lemon-900',
             },
             size: {
+                sm: 'h-7 rounded-md text-xs px-4',
                 default: 'h-10 px-4 py-2',
-                sm: 'h-9 rounded-md px-3',
-                lg: 'h-11 rounded-md px-8',
+                lg: 'h-12 rounded-md px-6 text-md',
                 icon: 'h-10 w-10',
             },
         },
@@ -36,7 +36,7 @@ const buttonVariants = cva(
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
         VariantProps<typeof buttonVariants> {
-    asChild?: boolean
+    asChild?: boolean,
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
