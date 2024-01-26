de272ebc772fc62c8e5111ed2cefc5d503b84833
zignaggo <jvz1@aluno.ifal.edu.br>
2024-01-26 19:08:38 -0300

    fix:  image and remotepatterns

--- components/cards/anime.tsx
+++ components/cards/anime.tsx
@@ -12,21 +12,23 @@ type AnimeCardProps = {
 export function AnimeCard({
     title,
     subtitle,
+    url,
     highlight,
     onClick,
 }: AnimeCardProps) {
     return (
         <div
-            className="relative flex flex-col justify-between w-[250px] h-[350px] rounded-lg bg-[url('https://br.web.img3.acsta.net/pictures/19/09/16/17/09/4903250.jpg')] bg-cover hover:outline outline-purple-500 cursor-pointer shadow-lg active:shadow-purple-400/40"
+            className="relative flex flex-col justify-between w-[250px] h-[350px] rounded-lg bg-cover hover:outline outline-purple-500 cursor-pointer shadow-lg active:shadow-purple-400/40 overflow-hidden"
             onClick={onClick}
         >
-            <div className="flex items-start gap-1 p-3">
+            <Image className="absolute z-[0]" layout='fill' alt="anime-image" objectFit="cover" src={url} />
+            <div className="flex items-start gap-1 p-3 z-[1]">
                 <DubLegBadge type="dub" variant={'default'}>
                     DUB
                 </DubLegBadge>
                 <DubLegBadge type="leg">LEG</DubLegBadge>
-            </div>
-            <div className="flex flex-col p-3 items-start justify-end min-h-[120px] w-full bg-gradient-to-t from-black-950 from-10% select-none">
+            I</div>
+            <div className="flex flex-col p-3 items-start justify-end min-h-[120px] w-full bg-gradient-to-t from-black-950 from-10% select-none z-[1]">
                 <h4 className="text-[20px] font-bold">{title}</h4>
                 <div className="flex justify-between w-full">
                     <p className="text-black-400">{subtitle}</p>
