eca4168ba86724dab6ac1c82c34051bfede21b21
zignaggo <jvz1@aluno.ifal.edu.br>
2024-04-21 14:01:26 -0300

    fix: remove old implementation

--- app/api/animes/route.ts
+++
@@ -1,7 +0,0 @@
-import { ANIME } from "@consumet/extensions"
-
-export async function GET(request: Request) {
-    const searchEngine = new ANIME.Anify()
-    const response = await searchEngine.search('jujutsu')
-    return Response.json(response)
-}
