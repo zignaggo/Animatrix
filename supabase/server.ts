8418ca2cc8cfc3d28a50fc8dc6f05f7aca9452b6
zignaggo <jvz1@aluno.ifal.edu.br>
2024-04-23 23:54:49 -0300

    fix: remove NEXT_PUBLIC

--- supabase/server.ts
+++ supabase/server.ts
@@ -30,8 +30,8 @@ export function createClient() {
         },
     }
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!,
         {
             cookies,
         }
