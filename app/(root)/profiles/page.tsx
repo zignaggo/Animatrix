18371e2728b6b756f2f0ecd48783a50a38ac7dfd
zignaggo <jvz1@aluno.ifal.edu.br>
2024-05-13 16:47:29 -0300

    fix: build

--- app/(root)/profiles/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getProfiles } from '@/components/pages/profiles/getProfiles'
-import { createClient } from '@/supabase/server'
-
-export default async function Profiles() {
-    const supabase = createClient()
-    const response = await getProfiles(supabase)
-    return response.map((profile) => <div key={profile.id}>{profile.name}</div>)
-}
