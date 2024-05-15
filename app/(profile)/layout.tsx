5a384a372fea6dcced20f4ba60a419d5c1130e39
zignaggo <jvz1@aluno.ifal.edu.br>
2024-05-15 17:10:53 -0300

    fix: remove useless code, and fix profile page mobile responsivity

--- app/(profile)/layout.tsx
+++ app/(profile)/layout.tsx
@@ -1,15 +1,8 @@
-import { createClient } from '@/supabase/server'
-import { redirect } from 'next/navigation'
 export default async function Layout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
-    const supabase = createClient()
-    const { data, error } = await supabase.auth.getUser()
-    if (error || !data.user) {
-        return redirect('/auth/sign')
-    }
     return (
         <main className="flex h-svh bg-background">
             <section className="flex flex-grow flex-col items-center justify-center max-h-screen overflow-y-auto custom-scroll">
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -3,11 +3,12 @@ export default async function Layout({
 }: Readonly<{
     children: React.ReactNode
 }>) {
-    // minmax(340px, 666px)
     return (
-        <main className="grid grid-cols-[3fr_666px] h-svh">
-            <section className="bg-black-900"></section>
-            <section className="p-10 flex items-center justify-center bg-black-950">{children}</section>
+        <main className="grid grid-cols-1 md:grid-cols-[3fr_666px] h-svh">
+            <section className="bg-black-900 hidden md:flex"></section>
+            <section className="p-10 flex items-center justify-center bg-black-950">
+                {children}
+            </section>
         </main>
     )
 }
