8fe83c776b649042219be329bc93349ceafea2d8
zignaggo <jvz1@aluno.ifal.edu.br>
2024-05-23 09:06:09 -0300

    feat: animes details

--- lib/myanimelist/index.ts
+++ lib/myanimelist/index.ts
@@ -1,6 +1,13 @@
-import { ResponseSeasonalAnime } from './types'
+import {
+    ErrorSeasonalAnime,
+    ResponseAnimeDetails,
+    ResponseSeasonalAnime,
+} from '@/lib/myanimelist/types'
 import { envServerSchema } from '@/types/serverEnvSchema'
 const api = 'https://api.myanimelist.net/v2'
+const headers = {
+    'X-MAL-CLIENT-ID': envServerSchema.X_MAL_CLIENT_ID,
+}
 export async function getSeasonalAnime(
     year: number,
     season: 'winter' | 'spring' | 'summer' | 'fall'
@@ -8,11 +15,28 @@ export async function getSeasonalAnime(
     const response = await fetch(
         `${api}/anime/season/${year}/${season}?fields="popularity, num_episodes, status"`,
         {
-            headers: {
-                'X-MAL-CLIENT-ID': envServerSchema.X_MAL_CLIENT_ID,
-            },
+            headers,
         }
     )
     const data = await response.json()
     return data
+}
+export async function getAnimeDetails(
+    id: number
+): Promise<ResponseAnimeDetails | ErrorSeasonalAnime> {
+    try {
+        const response = await fetch(
+            `${api}/anime/${id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics`,
+            {
+                headers,
+            }
+        )
+        const data = await response.json()
+        if (data.error) {
+            return { error: true, text: 'Erro ao encontrar o anime' }
+        }
+        return { data, error: false }
+    } catch (error) {
+        return { error: true, text: 'Erro ao encontrar o anime' }
+    }
 }
--- lib/myanimelist/types.ts
+++ lib/myanimelist/types.ts
@@ -1,13 +1,64 @@
-export type Anime = {
+export type Days =
+    | 'sunday'
+    | 'monday'
+    | 'tuesday'
+    | 'thursday'
+    | 'wednesday'
+    | 'friday'
+    | 'saturday'
+export type seasons = 'winter' | 'spring' | 'summer' | 'fall'
+export type genders =
+    | 'romance'
+    | 'action'
+    | 'adventure'
+    | 'comedy'
+    | 'drama'
+    | 'fantasy'
+    | 'horror'
+    | 'magic'
+    | 'mystery'
+    | 'psychological'
+    | 'sci-fi'
+    | 'slice of life'
+    | 'sports'
+    | 'supernatural'
+    | 'thriller'
+    | 'yaoi'
+    | 'yuri'
+    | 'winter'
+type picture = {
+    medium: string
+    large: string
+}
+type status = 'finished_airing' | 'currently_airing' | 'not_yet_aired'
+type Studio = { id: number; name: string }
+export type BaseAnime = {
     id: number
     title: string
-    main_picture: {
-        medium: string
-        large: string
-    }
+    main_picture: picture
+}
+export type Anime = BaseAnime & {
     popularity: number
     num_episodes: number
-    status: 'finished_airing' | 'currently_airing' | 'not_yet_aired' // Finalizado, Ocorrendo, Vai lançar
+    synopsis: string
+    alternative_titles: {
+        synonyms: string[]
+        en: string
+        ja: string
+    }
+    start_season: {
+        year: number
+        season: seasons
+    }
+    start_date: string
+    end_date: string
+    genres: { id: number; name: genders }[]
+    status: status // Finalizado, Ocorrendo, Vai lançar
+    pictures: picture[]
+    mean: number // avaliação 7.87
+    broadcast: { day_of_the_week: Days; start_time: string } // Dia e hora do lançamento do anime no japão
+    recommendations: { node: BaseAnime; num_recommendations: number }
+    studios: Studio[]
 }
 export type ResponseSeasonalAnime = {
     data: { node: Anime }[]
@@ -15,3 +66,8 @@ export type ResponseSeasonalAnime = {
     paging: string
 }
 export type ErrorSeasonalAnime = { error: true; text: string }
+
+export type ResponseAnimeDetails = {
+    data: Anime
+    error: false
+}
