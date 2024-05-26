4281f32121828ddd73fd2132280ef97d69f0fb04
zignaggo <jvz1@aluno.ifal.edu.br>
2024-05-26 20:31:51 -0300

    fix: signUp

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -6,7 +6,6 @@ import { createSafeUser } from '@/server/actions/auth/register'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {
-    Form,
     FormControl,
     FormField,
     FormItem,
@@ -17,10 +16,10 @@ import { useAction } from 'next-safe-action/hooks'
 import { useToast } from '@/components/ui/use-toast'
 import * as z from 'zod'
 import { registerSchema } from '@/server/actions/auth/register/schema'
-import { useRouter } from 'next-nprogress-bar';
+import { useRouter } from 'next-nprogress-bar'
 export default function Register() {
     const { toast } = useToast()
-    const route = useRouter();
+    const route = useRouter()
     const form = useForm<z.infer<typeof registerSchema>>({
         resolver: zodResolver(registerSchema),
         defaultValues: {
@@ -51,79 +50,75 @@ export default function Register() {
         execute(values)
     }
     return (
-        <Form {...form}>
-            <AuthLayout
-                subtitle="Crie sua conta e desfrute da plataforma"
-                register={true}
-                loading={status === 'executing'}
-                onSubmit={form.handleSubmit(onSubmit)}
-                disabled={status === 'hasSucceeded'}
-            >
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Nome de usuário</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="ex: Spam Caxota"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>E-mail</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="ex: Jorge@gmail.com"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Senha</FormLabel>
-                            <FormControl>
-                                <PasswordInput
-                                    placeholder="ex: coxinha123"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="confirmPassword"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Confirmar senha</FormLabel>
-                            <FormControl>
-                                <PasswordInput
-                                    placeholder="ex: coxinha123"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-            </AuthLayout>
-        </Form>
+        <AuthLayout
+            subtitle="Crie sua conta e desfrute da plataforma"
+            register={true}
+            loading={status === 'executing'}
+            onSubmit={form.handleSubmit(onSubmit)}
+            disabled={status === 'hasSucceeded'}
+            form={form}
+        >
+            <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Nome de usuário</FormLabel>
+                        <FormControl>
+                            <Input placeholder="ex: Spam Caxota" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>E-mail</FormLabel>
+                        <FormControl>
+                            <Input
+                                placeholder="ex: Jorge@gmail.com"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Senha</FormLabel>
+                        <FormControl>
+                            <PasswordInput
+                                placeholder="ex: coxinha123"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Confirmar senha</FormLabel>
+                        <FormControl>
+                            <PasswordInput
+                                placeholder="ex: coxinha123"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+        </AuthLayout>
     )
 }
