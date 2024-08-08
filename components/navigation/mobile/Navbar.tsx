427b459869d87764cceb05b9d5e6fd52f01e5ae0
zignaggo <jvz1@aluno.ifal.edu.br>
2024-08-07 22:57:41 -0300

    fix: remove old code

--- components/navigation/mobile/Navbar.tsx
+++ components/navigation/mobile/Navbar.tsx
@@ -1,11 +1,9 @@
 import { IconButton } from '@/components/ui/icon-button'
 import Icon from '@/components/ui/icons'
 import { SidebarItem } from '../sidebar/Item'
-import { UserInfo } from '../sidebar/User'
 export function Navbar() {
     return (
         <header className="px-6 py-3 flex gap-2 w-100 flex-row">
-            <UserInfo />
             <nav className="flex flex-1 justify-end gap-2">
                 <SidebarItem
                     href="favorite"
